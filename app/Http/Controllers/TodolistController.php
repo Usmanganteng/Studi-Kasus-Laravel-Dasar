f75fdb63adc0c37930e97624cb525ee5991676e5
lucian666 <ilhamaldizar@gmail.com>
2024-04-28 22:40:25 +0700

    have completed a basic laravel case study

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\TodolistService;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class TodolistController extends Controller
+{
+
+    private TodolistService $todolistService;
+
+    public function __construct(TodolistService $todolistService)
+    {
+        $this->todolistService = $todolistService;
+    }
+
+    public function todoList(Request $request)
+    {
+        $todolist = $this->todolistService->getTodolist();
+        return response()->view("todolist.todolist", [
+            "title" => "Todolist",
+            "todolist" => $todolist
+        ]);
+    }
+
+    public function addTodo(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'todo' => 'required|string|min:2',
+        ]);
+
+        if ($validator->fails()) {
+            $todolist = $this->todolistService->getTodolist();
+            return response()->view("todolist.todolist", [
+                "title" => "Todolist",
+                "todolist" => $todolist,
+                "error" => $validator->errors()->first(),
+            ]);
+        }
+
+        $this->todolistService->saveTodo(uniqid(), $request->input("todo"));
+
+        return redirect()->action([TodolistController::class, 'todoList']);
+    }
+
+    public function removeTodo(Request $request, string $todoId): RedirectResponse
+    {
+        $this->todolistService->removeTodo($todoId);
+        return redirect()->action([TodolistController::class, 'todoList']);
+    }
+
+}
